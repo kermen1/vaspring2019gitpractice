d783e175594443becea803a0133776c3292ee318
kermen1 <kermenkurs@gmail.com>
2019-08-06 20:52:38 -0400

    adding second file

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,6 +2,8 @@ public class LoginTest {
     public static void main(String[] args) {
         System.out.println("line 1");
         System.out.println("line 2");
-        System.out.println("line 3git ");
+        System.out.println("line 3");
+        System.out.println("line 4");
+
     }
 }
