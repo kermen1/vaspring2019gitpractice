27d9dd27e0300f3062f5382432c873a4d8505f1c
kermen1 <kermenkurs@gmail.com>
2019-08-13 19:44:47 -0400

    ''

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,8 +2,8 @@ public class LoginTest {
     public static void main(String[] args) {
         System.out.println("line 1");
         System.out.println("line 27878");
-        System.out.println("line 3354");
-        System.out.println("line 311");
+        System.out.println("line 35656354");
+        System.out.println("line 346411");
         System.out.println("line 4");
 
     }
